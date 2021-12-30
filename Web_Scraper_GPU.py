43e7610e406e88bbbb8d5f564b2aa0507780a757
ethan <ethanhochy@gmail.com>
2021-12-30 15:09:31 +0000

    print when a website has no items
    deal with website page having no items
    
    deal with overclocker not passing cloudflare

--- Web_Scraper_GPU.py
+++ Web_Scraper_GPU.py
@@ -6,19 +6,11 @@ import re
 from playwright.sync_api import sync_playwright
 from time import sleep
 
-# User must specify if they want an AMD or Nvidia GPU as some websites separate them out
-# input_track = True
-# while input_track:
-#     brand = input("Do you want to search for an AMD or Nvidia GPU?")
-#     if brand.lower() == "amd" or brand.lower() == "nvidia":
-#         input_track = False
-#     else:
-#         input_track = True
 product = input("What product do you want to search for?")
 
 # initialise dictionary where all GPUs found will be stored
 items_found = {}
-
+found = False
 
 # newegg.com
 # url = f"https://www.newegg.com/global/uk-en/p/pl?d={product}" - generic search.
@@ -58,14 +50,20 @@ for page in range(1, pages + 1):
         price_pence = next_parent.find(class_="price-current").sup.string
         price = price_pound + price_pence
         items_found[item] = {"price": float(price.replace(",", "")), "link": link}
+        found = True
+
+if not found:
+    print("No items found on www.newegg.com")
 
 
+found = False
 # ebuyer.com
 # url = f"https://www.ebuyer.com/search?q={product}" - generic search.
 # search specifically for graphics cards - both AMD and Nvidia
-url = []
+url = list()
 url.append(f"https://www.ebuyer.com/store/Components/cat/Graphics-Cards-Nvidia?q={product}")
 url.append(f"https://www.ebuyer.com/store/Components/cat/Graphics-Cards-AMD?q={product}")
+print(url)
 
 for i in range(2):
     page = requests.get(url[i]).text
@@ -75,41 +73,50 @@ for i in range(2):
     # number of pages not shown so need to calculate it from number of products shown and available to view.
     page_text = doc.find(class_="showing-count")
     pages_text = str(page_text).split("of")
-    if len(pages_text) > 1:
-        items_page = int(pages_text[0].replace(" ", "").split("-")[2])
-        total_items = int(pages_text[1].replace(" ", "").split("r")[0])
-        pages = (total_items + (items_page - 1))//items_page
+    if not page_text:
+        continue
     else:
-        pages = int(pages_text[0].split(" ")[2])
-    for page in range(1, pages + 1):
-        if i == 0:
-            url = f"https://www.ebuyer.com/store/Components/cat/Graphics-Cards-Nvidia?page={page}&q={product}"
+        if len(pages_text) > 1:
+            items_page = int(pages_text[0].replace(" ", "").split("-")[2])
+            total_items = int(pages_text[1].replace(" ", "").split("r")[0])
+            pages = (total_items + (items_page - 1))//items_page
         else:
-            url = f"https://www.ebuyer.com/store/Components/cat/Graphics-Cards-AMD?page={page}&q={product}"
+            pages = int(pages_text[0].split(" ")[2])
+        for page in range(1, pages + 1):
+            if i == 0:
+                url = f"https://www.ebuyer.com/store/Components/cat/Graphics-Cards-Nvidia?page={page}&q={product}"
+            else:
+                url = f"https://www.ebuyer.com/store/Components/cat/Graphics-Cards-AMD?page={page}&q={product}"
 
-        page = requests.get(url).text
-        doc = BeautifulSoup(page, "html.parser")
+            page = requests.get(url).text
+            doc = BeautifulSoup(page, "html.parser")
 
-        # only items within grid
-        div = doc.find(class_="grid-view js-taxonomy-view is-active")
-        # find any text with searched product terms
-        items = div.find_all(text=re.compile(product))
-        for item in items:
-            parent = item.parent
-            if parent.name != "a":
-                continue
-            next_parent = item.find_parent(class_="grid-item js-listing-product")
-            # checks if the item is out of stock
-            if next_parent.find(class_="grid-item__coming-soon"):
-                continue
-            link = parent['href']
-            link = "https://www.ebuyer.com" + link
-            price = next_parent.find(class_="grid-item__price").find(text=re.compile(" "))
-            item = str(item).strip()
-            price = price.strip()
-            items_found[item] = {"price": float(price.replace(",", "")), "link": link}
+            # only items within grid
+            div = doc.find(class_="grid-view js-taxonomy-view is-active")
+            # find any text with searched product terms
+            items = div.find_all(text=re.compile(product))
+            for item in items:
+                parent = item.parent
+                if parent.name != "a":
+                    continue
+                next_parent = item.find_parent(class_="grid-item js-listing-product")
+                # checks if the item is out of stock
+                if next_parent.find(class_="grid-item__coming-soon"):
+                    continue
+                link = parent['href']
+                link = "https://www.ebuyer.com" + link
+                price = next_parent.find(class_="grid-item__price").find(text=re.compile(" "))
+                item = str(item).strip()
+                price = price.strip()
+                items_found[item] = {"price": float(price.replace(",", "")), "link": link}
+                found = True
+
+if not found:
+    print("No items found on www.newegg.com")
 
 
+found = False
+opened = False
 # overclockers.co.uk
 # can fail to reach server due to cloudflare so try up to 4 times
 for x in range(0, 4):
@@ -129,7 +136,10 @@ for x in range(0, 4):
         page.wait_for_timeout(10000)
         doc = BeautifulSoup(page.content(), "html.parser")
         browser.close()
+        opened = True
 
+    if not opened:
+        continue
     # find number of pages to search through them all
     page_text = doc.find(class_="display_sites").contents
     pages = int(page_text[7].string)
@@ -158,13 +168,15 @@ for x in range(0, 4):
                 continue
             price = float(price.find(text=re.compile("£")).replace("£", "").replace(",", "").strip())
             items_found[item['title']] = {"price": price, "link": link}
+            found = True
     error = False
     if error:
         sleep(2)  # wait 2 seconds if failed
     else:
         break
 
-
+if not found:
+    print("No items found on www.newegg.com")
 
 sorted_items = sorted(items_found.items(), key=lambda x: x[1]['price'])
 
